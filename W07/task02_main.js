c82fe648c8c49dfab1b2b0811dd1ced335917de6
mmousset <moussetmaxime09@gmail.com>
2023-11-22 08:31:56 +0900

    commit tasks

---
+++ W07/task02_main.js
@@ -0,0 +1,111 @@
+class ScatterPlot {
+
+    constructor(config, data) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || { top: 10, right: 10, bottom: 10, left: 10 }
+        };
+        this.data = data;
+        this.init();
+    }
+
+    init() {
+        let self = this;
+        let ticks = 7;
+        let marginTitles = 20;
+
+        self.svg = d3.select(self.config.parent)
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+        self.chart = self.svg.append('g')
+            .attr('transform',
+                `translate(${self.config.margin.left + marginTitles}, ${self.config.margin.top + marginTitles})`);
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right - marginTitles;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom - marginTitles*2;
+
+        self.xscale = d3.scaleLinear()
+            .range([0, self.inner_width]);
+        self.yscale = d3.scaleLinear()
+            .range([0, self.inner_height]);
+
+        self.xaxis = d3.axisBottom(self.xscale)
+            .ticks(ticks);
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
+        self.yaxis = d3.axisLeft(self.yscale)
+            .ticks(ticks);
+        self.yaxis_group = self.chart.append('g');
+
+        self.svg.append("text")
+            .attr("x", ((self.inner_width / 2) + self.config.margin.left + marginTitles))
+            .attr("y", marginTitles)
+            .attr("text-anchor", "middle")
+            .style("font-size", "16px")
+            .style("font-weight", "bold")
+            .text("W07 - f(x)");
+
+        self.svg.append("text")
+            .attr("x", self.config.width / 2)
+            .attr("y", self.config.height)
+            .style("text-anchor", "middle")
+            .style("font-size", "12px")
+            .style("font-weight", "bold")
+            .text("W07 - X Axis");
+
+        self.svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("x", 0 - (self.inner_height / 2 + marginTitles))
+            .attr("y", 0)
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .style("font-size", "12px")
+            .style("font-weight", "bold")
+            .text("W07 - Y Axis");
+
+    }
+
+    update() {
+        let self = this;
+        let xaxisMarge = 20;
+        let yaxisMarge = 20;
+
+        const xmin = d3.min(self.data, d => d.x) - xaxisMarge;
+        const xmax = d3.max(self.data, d => d.x) + xaxisMarge;
+        self.xscale.domain([xmin, xmax]);
+        const ymin = d3.min(self.data, d => d.y) - yaxisMarge;
+        const ymax = d3.max(self.data, d => d.y) + yaxisMarge;
+        self.yscale.domain([ymax, ymin]);
+        self.render();
+    }
+
+    render() {
+        let self = this;
+        self.chart.selectAll("circle")
+            .data(self.data)
+            .enter()
+            .append("circle")
+            .attr("cx", d => self.xscale(d.x))
+            .attr("cy", d => self.yscale(d.y))
+            .attr("r", d => d.r);
+        self.xaxis_group
+            .call(self.xaxis);
+        self.yaxis_group
+            .call(self.yaxis);
+    }
+}
+
+d3.csv("https://mmousset.github.io/InfoVis2023/W04/data.csv")
+    .then(data => {
+        data.forEach(d => { d.x = +d.x; d.y = +d.y; });
+        var config = {
+            parent: '#drawing_region',
+            width: 256,
+            height: 256,
+            margin: { top: 10, right: 10, bottom: 20, left: 25 }
+        };
+        const scatter_plot = new ScatterPlot(config, data);
+        scatter_plot.update();
+    })
+    .catch(error => { console.log(error); });
