9e22f36aeba8ccf928019950cacaef5fc107a5ba
mmousset <moussetmaxime09@gmail.com>
2023-11-30 12:17:57 +0900

    WIP

--- W08/w08_task01_main.js
+++ W08/w08_task01_main.js
@@ -24,43 +24,42 @@ class barChart {
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
         self.xscale = d3.scaleLinear()
+            .domain([0, d3.max(self.data, d => d.value)])
             .range([0, self.inner_width]);
         self.yscale = d3.scaleBand()
-            .range([0, self.inner_height]);
+            .domain(self.data.map(d => d.label))
+            .range([0, self.inner_height])
+            .paddingInner(0.1);
 
-        self.xaxis = d3.axisBottom(self.xscale)
-            .ticks(ticks);
+        self.xaxis = d3.axisBottom(self.xscale);
         self.xaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, ${self.inner_height})`);
-        self.yaxis = d3.axisLeft(self.yscale)
-            .ticks(ticks);
+        self.yaxis = d3.axisLeft(self.yscale);
         self.yaxis_group = self.chart.append('g');
 
     }
 
     update() {
         let self = this;
-        let xaxisMarge = 20;
-        let yaxisMarge = 20;
 
-        const xmin = d3.min(self.data, d => d.x) - xaxisMarge;
-        const xmax = d3.max(self.data, d => d.x) + xaxisMarge;
+        const xmin = d3.min(self.data, d => d.x);
+        const xmax = d3.max(self.data, d => d.x);
         self.xscale.domain([xmin, xmax]);
-        const ymin = d3.min(self.data, d => d.y) - yaxisMarge;
-        const ymax = d3.max(self.data, d => d.y) + yaxisMarge;
+        const ymin = d3.min(self.data, d => d.y);
+        const ymax = d3.max(self.data, d => d.y);
         self.yscale.domain([ymin, ymax]);
         self.render();
     }
 
     render() {
         let self = this;
-        self.chart.selectAll("circle")
-            .data(self.data)
-            .enter()
-            .append("circle")
-            .attr("cx", d => self.xscale(d.x))
-            .attr("cy", d => self.yscale(d.y))
-            .attr("r", d => d.r);
+
+        self.chart.selectAll("rect").data(self.data).enter()
+            .append("rect")
+            .attr("x", 0)
+            .attr("y", d => self.yscale(d.label))
+            .attr("width", d => self.xscale(d.value))
+            .attr("height", self.yscale.bandwidth());
         self.xaxis_group
             .call(self.xaxis);
         self.yaxis_group
@@ -76,9 +75,9 @@ d3.csv("https://mmousset.github.io/InfoVis2023/W08/data.csv")
             parent: '#drawing_region',
             width: 256,
             height: 256,
-            margin: { top: 10, right: 10, bottom: 20, left: 25 }
+            margin: { top: 10, right: 10, bottom: 10, left: 10 }
         };
-        const scatter_plot = new ScatterPlot(config, data);
-        scatter_plot.update();
+        const bar_chart = new barChart(config, data);
+        bar_chart.update();
     })
     .catch(error => { console.log(error); });
