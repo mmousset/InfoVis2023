50017a0f271be259848bced8cf1136700d01316d
mmousset <moussetmaxime09@gmail.com>
2023-12-06 18:20:55 +0900

    new datat

--- W08/w08_task02_main.js
+++ W08/w08_task02_main.js
@@ -1,4 +1,4 @@
-class barChart {
+class lineChart {
 
     constructor(config, data) {
         this.config = {
@@ -21,6 +21,7 @@ class barChart {
         self.chart = self.svg.append('g')
             .attr('transform',
                 `translate(${self.config.margin.left + marginLabels}, ${self.config.margin.top})`);
+
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right - self.config.margin.left - marginLabels;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom - self.config.margin.top;
 
@@ -46,40 +47,48 @@ class barChart {
         const xmin = d3.min(self.data, d => d.value);
         const xmax = d3.max(self.data, d => d.value);
         self.xscale.domain([xmin, xmax]);
-        self.yscale = d3.scaleBand()
-            .domain(self.data.map(d => d.label))
-            .range([0, self.inner_height])
-            .paddingInner(0.1);
+        const ymin = d3.min(self.data, d => d.y) - yaxisMarge;
+        const ymax = d3.max(self.data, d => d.y) + yaxisMarge;
+        self.yscale.domain([ymin, ymax]);
         self.render();
     }
 
     render() {
         let self = this;
-        
-        self.chart.selectAll("rect").data(self.data).enter()
-            .append("rect")
-            .attr("x", 0)
-            .attr("y", d => self.yscale(d.label))
-            .attr("width", d => self.xscale(d.value))
-            .attr("height", self.yscale.bandwidth());
-        self.xaxis_group
-            .call(self.xaxis);
-        self.yaxis_group
-            .call(self.yaxis);
+
+        const line = d3.line()
+            .x(d => d.x)
+            .y(d => d.y);
+
+        self.svg.append('path')
+            .attr('d', line(data))
+            .attr('stroke', 'black')
+            .attr('fill', 'none');
+
+        // self.chart.selectAll("rect").data(self.data).enter()
+        //     .append("rect")
+        //     .attr("x", 0)
+        //     .attr("y", d => self.yscale(d.label))
+        //     .attr("width", d => self.xscale(d.value))
+        //     .attr("height", self.yscale.bandwidth());
+        // self.xaxis_group
+        //     .call(self.xaxis);
+        // self.yaxis_group
+        //     .call(self.yaxis);
     }
 
 }
 
-d3.csv("https://mmousset.github.io/InfoVis2023/W08/data.csv")
+d3.csv("https://mmousset.github.io/InfoVis2023/W08/data2.csv")
     .then(data => {
-        data.forEach(d => { d.value = +d.value; });
+        data.forEach(d => { d.x = +d.x; d.y = +d.y; });
         var config = {
             parent: '#drawing_region',
             width: 512,
             height: 256,
             margin: { top: 10, right: 10, bottom: 10, left: 10 }
         };
-        const bar_chart = new barChart(config, data);
-        bar_chart.update();
+        const line_chart = new lineChart(config, data);
+        line_chart.update();
     })
     .catch(error => { console.log(error); });    
